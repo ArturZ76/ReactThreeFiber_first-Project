7d41a7dab777f63ce7198777f32a0784d1eacaf3
Artur <artur.zajaczkowski76@gmail.com>
2026-05-02 17:19:15 +0200

    initial commit

---
+++ first-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { Canvas } from "@react-three/fiber";
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div >
+      <Canvas style={{ backgroundColor: 'deepskyblue' }}>
+
+      </Canvas>
+    </div>
+  )
+}
+
+export default App
